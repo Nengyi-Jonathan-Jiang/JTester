acb782ef7c94cd7a8631945a88e35208ef18e18d
Nengyi-Jonathan-Jiang <theNerdyMango@gmail.com>
2024-04-28 21:56:59 -0500

    :)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 import {generateQuiz} from "./jquiz.js";
 
-await generateQuiz(
-    document.getElementById('questions'),
-    './quizzes/java-test.jquiz'
-);
+let quiz = window.location.hash.replaceAll(/^#/g, '');
+try {
+    await generateQuiz(
+        document.getElementById('questions'),
+        `./quizzes/${quiz}.jquiz`
+    );
+}
+catch (e) {
+    alert(`Invalid quiz: "${quiz}" not found or broken.`)
+}
